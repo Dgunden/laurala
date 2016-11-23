90376b95261bb287a9cae041842d9096dfddecdb
dennis gunden <dennisgunden@gmail.com>
2016-11-23 21:29:29 +0000

    first commit

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+end
