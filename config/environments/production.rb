4461946d8140901b3b456d5b6f60a1e892c04756
dennis gunden <dennisgunden@gmail.com>
2017-04-07 22:41:42 +0000

    pushing to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 config.action_mailer.default_url_options = { host:'laurala.herokuapp.com'}
 end 
   # Code is not reloaded between requests.
-  #config.cache_classes = true
+  econfig.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
