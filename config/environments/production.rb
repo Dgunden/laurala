ab93f5a75d36a5d041ad4def3c662e4326c6de89
dennis gunden <dennisgunden@gmail.com>
2017-04-08 16:24:44 +0000

    common and push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ config.action_mailer.default_url_options = { host:'laurala.herokuapp.com'}
 end 
   # Code is not reloaded between requests.
   config.cache_classes = true  
-
+end 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
