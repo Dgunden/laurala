b37ccad75a26fc0a3413501b61696825036ff4dd
dennis gunden <dennisgunden@gmail.com>
2017-04-18 20:34:49 +0000

    update and pushing

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,9 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, except: [:index, :show]
+before_action :correct_user, only: [:edit, :update, :destroy]
+
+respond_to :html 
 
   # GET /pins
   # GET /pins.json
@@ -70,5 +74,9 @@ class PinsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
-    end
+    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if@pin.nil?
+  end
 end
